bfaa5b796561ee892670d024da54be8ef5e50b06
Даниил Хохлов <xoxloff.daniil72@yandex.ru>
2020-07-03 19:50:08 +0500

    Added exit panel

---
+++ Assets/Scripts/LayoutManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LayoutManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject factoryRef;
+    [SerializeField]
+    private GameObject manufacturePrefab;
+    [SerializeField]
+    private GameObject exitPanel;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Application.platform == RuntimePlatform.Android && Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnClickExitButton();
+        }
+    }
+
+    public void AddManufacturePrefabToFactory()
+    {
+        GameObject newChild = new GameObject();
+        if (factoryRef != null && manufacturePrefab != null)
+        {
+            newChild = Instantiate(manufacturePrefab, factoryRef.transform);
+        }
+
+        newChild.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = (factoryRef.transform.childCount).ToString();
+    }
+
+    public void OnClickExitButton()
+    {
+        exitPanel.SetActive(!exitPanel.activeSelf);
+    }
+
+    public void ClosePopupExitPanel()
+    {
+        exitPanel.SetActive(false);
+    }
+
+    public void QuiteGame()
+    {
+        Application.Quit();
+    }
+}
