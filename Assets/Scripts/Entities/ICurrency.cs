dc2916afb7d019661b3c6752bb815525d213f71f
Никита Лобаков <lobakov.nek@gmail.com>
2020-07-04 22:51:57 +0500

    add currency entities

---
+++ Assets/Scripts/Entities/ICurrency.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.UI;
+
+public interface ICurrency
+{
+    public int Id { get; }
+    public Image Image { get; set; }
+    public string Name { get; set; }
+    public ShortBigInteger Value { get; set; }
+}
---
+++ Assets/Scripts/Entities/ScientistCurrency.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScientistCurrency : MonoBehaviour, ICurrency
+{
+    public ScientistCurrency(int id)
+    {
+        Id = id;
+    }
+    public ScientistCurrency(int id, string name) : this(id)
+    {
+        Name = name;
+    }
+
+    public int Id { get; private set; }
+    public Image Image { get; set; }
+    public string Name { get; set; }
+    public ShortBigInteger Value { get; set; }
+}
