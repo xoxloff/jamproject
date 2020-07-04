f56c51fc74ec6816f5f51999cfe1482d26df3fa7
Даниил Хохлов <xoxloff.daniil72@yandex.ru>
2020-07-04 22:19:52 +0500

    Added scripts

---
+++ Assets/Scripts/Entities/ManufactureModel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ManufactureModel : MonoBehaviour
+{
+
+    public Image manufactureIcon;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
