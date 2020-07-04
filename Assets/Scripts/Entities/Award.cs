75ccb7e8c032f60873ab6a60b909810759da6786
Даниил Хохлов <xoxloff.daniil72@yandex.ru>
2020-07-04 22:52:03 +0500

    Manufacture update

--- Assets/Scripts/Entities/Award.cs
+++ Assets/Scripts/Entities/Award.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Award : MonoBehaviour
 {
+    [SerializeField]
+    private int scientificCurrency;
+    [SerializeField]
+    private List<Scientist> scientistsCards;
+    [SerializeField]
+    private int scientificCurrencyLimit;
+    [SerializeField]
+    private int scientistsCardLimit;
+
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Entities/Manufacture.cs
+++ Assets/Scripts/Entities/Manufacture.cs
@@ -3,9 +3,29 @@ using UnityEngine.UI;
 
 public class Manufacture : MonoBehaviour
 {
+    [SerializeField]
+    private Image icon;
+    [SerializeField]
+    private ShortBigInteger currentCountOfCurrency;
+    [SerializeField]
+    private ShortBigInteger additionalCountOfCurrency;
+    [SerializeField]
+    private ShortBigInteger currentCurrencyMultiplier;
+    [SerializeField]
+    private ShortBigInteger additionalCurrencyMultiplier;
+    [SerializeField]
+    private double timeForCreate;
+    [SerializeField]
+    private double timeMultiplier;
+    [SerializeField]
+    private ShortBigInteger cost;
+    [SerializeField]
+    private Scientist scientistCard;
+    [SerializeField]
+    private int scientificCurrency;
+    [SerializeField]
+    private int scientificTrigger;
 
-    public Image manufactureIcon;
-    
     void Start()
     {
         
