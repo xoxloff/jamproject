7a6a47f0d0b3224ff09dfe4211a77d2dd1be3d2b
Даниил Хохлов <xoxloff.daniil72@yandex.ru>
2020-07-04 23:18:50 +0500

    Change Factory Manufacture scripts

--- Assets/Scripts/Entities/Factory.cs
+++ Assets/Scripts/Entities/Factory.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Factory : MonoBehaviour
 {
+    [SerializeField]
+    private List<Manufacture> manufactures;
+    [SerializeField]
+    private ICurrency FactoryCurrency;
+    [SerializeField]
+    private enum purchaseMode { x1,x10,x100,max};
+
     // Start is called before the first frame update
     void Start()
     {
