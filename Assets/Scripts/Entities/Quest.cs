618e90faded7c5339f8b28d40ad2706af8d1b3cd
Никита Лобаков <lobakov.nek@gmail.com>
2020-07-04 23:05:58 +0500

    add quest entitie

--- Assets/Scripts/Entities/Quest.cs
+++ Assets/Scripts/Entities/Quest.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Quest(int id)
     {
-        
+        this.id = id;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private int id;
+    [SerializeField]
+    private int description;
+    [SerializeField]
+    private Image icon;
+    [SerializeField]
+    private int upgradeCost;
+    [SerializeField]
+    private int manufacture;
+    [SerializeField]
+    private int level;
+    [SerializeField]
+    private int performanceRatio;
 }
